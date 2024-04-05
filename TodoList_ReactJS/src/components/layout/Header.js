06a9e285e3ad38e26927414f02583fc0954f9e3a
info.buiduyanh@gmail.com <info.buiduyanh@gmail.com>
2024-04-05 11:30:08 +0700

    Vẫn bị lỗi khi xóa task

--- TodoList_ReactJS/src/components/layout/Header.js
+++ TodoList_ReactJS/src/components/layout/Header.js
@@ -3,7 +3,7 @@ class Header extends React.Component {
   render() {
     return (
       <header className="header-container">
-        <h1 className="header-title">Todo - List - Bùi Duy Anh</h1>
+        <h1 className="header-title">TodoList -Bùi Duy Anh</h1>
       </header>
     );
   }
